3a097950b5286f415536539730eb3e8d36049f4d
NoeDimitri <justind4@usf.edu>
2023-09-14 10:02:58 -0400

    other minor changes

--- gloomhaven companion app/ClientApp/src/services/api.service.ts
+++ gloomhaven companion app/ClientApp/src/services/api.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
   getSortedEntities(){
-    return this.http.get<any>('https://localhost:7229/api/GameEntities/sorted');
+    console.log(this.baseUrl + 'api/GameEntities/sorted');
+    return this.http.get<any>(this.baseUrl + 'api/GameEntities/sorted');
   }
 }
