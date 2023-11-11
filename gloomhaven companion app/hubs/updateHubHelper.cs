4dfab4ee15fdadba6efa2be18ca3f150b4b0b60b
NoeDimitri <71399587+NoeDimitri@users.noreply.github.com>
2023-11-11 09:26:30 -0500

    crap i screwed up last commit bad lol

---
+++ gloomhaven companion app/hubs/updateHubHelper.cs
@@ -0,0 +1,11 @@
+public class updateHelper : updateHelperInterface
+{
+    public void tempFunction()
+    {
+        // Idk do something here
+    }
+}
+public interface updateHelperInterface 
+{
+    void tempFunction();
+}
